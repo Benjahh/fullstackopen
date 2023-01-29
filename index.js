e4e79f09d3fd32e16e329be4ae444e4f8dcb34ec
Benjahh <lucasbenja09@hotmail.com>
2023-01-29 19:38:09 -0300

    updated

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.post('/persons', (request, response) => {
 
 })
 
-const PORT = process.env.PORT || 3001
+const PORT =  3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
